003d3b19f3a0e46a9d8be1b945bf1735cf2f392f
Karol Komorowski <karolkolinski01@gmail.com>
2025-06-09 21:23:13 +0200

    Zmodyfikowanie pool.js

--- src/db/pool.js
+++ src/db/pool.js
@@ -1,11 +1,17 @@
-const { Pool, types } = require('pg');
+const { Pool, types } = require("pg");
 
-types.setTypeParser(1114, str => str);
-types.setTypeParser(1184, str => str);
+types.setTypeParser(1114, (str) => str);
+types.setTypeParser(1184, (str) => str);
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  host: process.env.PGHOST,
+  port: Number(process.env.PGPORT),
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
