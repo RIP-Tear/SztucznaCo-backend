30b77f3467a5ba50195aefbda8096d7e9b6be7e7
Karol Komorowski <karolkolinski01@gmail.com>
2025-07-17 20:56:23 +0200

    dodanie do indexu resolvera content i profits

--- src/graphql/resolvers/index.js
+++ src/graphql/resolvers/index.js
@@ -1,22 +1,30 @@
 const blogQuery = require("./blogQuery");
 const servicesQuery = require("./servicesQuery");
 const robotDialogueQuery = require("./robotDialogueQuery");
+const serviceContentQuery = require("./serviceContentQuery");
+const serviceProfitsQuery = require("./serviceProfitsQuery");
 
 const blogMutation = require("./blogMutation");
 const servicesMutation = require("./servicesMutation");
 const robotDialogueMutation = require("./robotDialogueMutation");
+const serviceContentMutation = require("./serviceContentMutation");
+const serviceProfitsMutation = require("./serviceProfitsMutation");
 
 const resolvers = {
   Query: {
     ...blogQuery,
     ...servicesQuery,
     ...robotDialogueQuery,
+    ...serviceContentQuery,
+    ...serviceProfitsQuery,
   },
   Mutation: {
     ...blogMutation,
     ...servicesMutation,
     ...robotDialogueMutation,
-  }
+    ...serviceContentMutation,
+    ...serviceProfitsMutation,
+  },
 };
 
 module.exports = resolvers;
